c1314525819181457c1a6cdf11611b46ee6f0b15
sseen2 <kse10023@naver.com>
2025-06-16 13:49:59 +0900

    feat: 결제 관련 enum 구현
    - 배달방법, 결제수단, 결제상태, 라이더 요청사항 옵션 관련 Enum 구현

---
+++ src/main/java/com/idukbaduk/itseats/payment/entity/enums/DeliveryType.java
@@ -0,0 +1,8 @@
+package com.idukbaduk.itseats.payment.entity.enums;
+
+public enum DeliveryType {
+
+    DEFAULT,
+    ONLY,
+    ONE
+}
---
+++ src/main/java/com/idukbaduk/itseats/payment/entity/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.idukbaduk.itseats.payment.entity.enums;
+
+public enum PaymentMethod {
+
+    CARD,
+    KAKAO,
+    MEET
+}
