c5c5da41d6656b97e2460d200cb400995e08782a
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-16 17:44:52 +0900

    feat: 가맹점 관련 연관관게 매핑

--- src/main/java/com/idukbaduk/itseats/store/entity/Store.java
+++ src/main/java/com/idukbaduk/itseats/store/entity/Store.java
@@ -1,6 +1,7 @@
 package com.idukbaduk.itseats.store.entity;
 
 import com.idukbaduk.itseats.global.BaseEntity;
+import com.idukbaduk.itseats.member.entity.Member;
 import com.idukbaduk.itseats.store.entity.enums.BusinessStatus;
 import com.idukbaduk.itseats.store.entity.enums.StoreStatus;
 import jakarta.persistence.*;
@@ -16,6 +17,18 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id")
     private Long storeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_category_id", nullable = false)
+    private StoreCategory storeCategory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "franchise_id")
+    private Franchise franchise;
 
     @Column(name = "store_name", nullable = false)
     private String storeName;
