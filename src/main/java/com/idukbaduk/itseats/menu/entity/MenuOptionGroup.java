d47e457cbfe9ec2fac0c3949962dc017ccfcbffb
sseen2 <kse10023@naver.com>
2025-06-16 17:36:06 +0900

    feat: 메뉴 옵션 그룹 entity 연관관계 설정

--- src/main/java/com/idukbaduk/itseats/menu/entity/MenuOptionGroup.java
+++ src/main/java/com/idukbaduk/itseats/menu/entity/MenuOptionGroup.java
@@ -3,9 +3,12 @@ package com.idukbaduk.itseats.menu.entity;
 import com.idukbaduk.itseats.global.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +27,10 @@ public class MenuOptionGroup extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "opt_group_id")
     private Long optGroupId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 
     @Column(name = "opt_group_name", nullable = false)
     private String optGroupName;
