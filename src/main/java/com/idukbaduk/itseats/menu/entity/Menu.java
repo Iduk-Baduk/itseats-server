0eced278c0b1a43c2ae9fe083d86c763d083515f
sseen2 <kse10023@naver.com>
2025-06-16 15:37:27 +0900

    feat: 메뉴 entity 구현

---
+++ src/main/java/com/idukbaduk/itseats/menu/entity/Menu.java
@@ -0,0 +1,52 @@
+package com.idukbaduk.itseats.menu.entity;
+
+import com.idukbaduk.itseats.global.BaseEntity;
+import com.idukbaduk.itseats.menu.entity.enums.MenuStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "menu")
+public class Menu extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_id")
+    private Long menuId;
+
+    @Column(name = "menu_name", nullable = false)
+    private String menuName;
+
+    @Column(name = "menu_price", nullable = false)
+    private int menuPrice;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "menu_status", nullable = false)
+    private MenuStatus menuStatus;
+
+    @Column(name = "menu_rating")
+    private Float menuRating;
+
+    @Column(name = "menu_description")
+    private String menuDescription;
+
+    @Column(name = "menu_priority", nullable = false)
+    private int menuPriority;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+}
