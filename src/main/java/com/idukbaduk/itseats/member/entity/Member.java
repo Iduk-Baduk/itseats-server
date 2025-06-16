eae7864cc64ba80462833656d4863926282f7937
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-16 12:43:48 +0900

    feat: Member 엔티티 구현

---
+++ src/main/java/com/idukbaduk/itseats/member/entity/Member.java
@@ -0,0 +1,41 @@
+package com.idukbaduk.itseats.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_type")
+    private MemberType memberType;
+
+}
---
+++ src/main/java/com/idukbaduk/itseats/member/entity/MemberType.java
@@ -0,0 +1,8 @@
+package com.idukbaduk.itseats.member.entity;
+
+public enum MemberType {
+    CUSTOMER,
+    RIDER,
+    OWNER,
+    MANAGER
+}
