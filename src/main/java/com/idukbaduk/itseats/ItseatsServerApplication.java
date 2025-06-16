4406fe188b480353e26cab2fa9d03be1a861b17e
sseen2 <kse10023@naver.com>
2025-06-16 09:32:15 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/idukbaduk/itseats/ItseatsServerApplication.java
@@ -0,0 +1,13 @@
+package com.idukbaduk.itseats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItseatsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItseatsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idukbaduk/itseats/ItseatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idukbaduk.itseats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItseatsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
