3bfb3874cd3c80d73a3c5c0bbdba420fb54c9c53
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-16 16:13:04 +0900

    feat: Favorite 클래스 추가

---
+++ src/main/java/com/idukbaduk/itseats/member/entity/Favorite.java
@@ -0,0 +1,22 @@
+package com.idukbaduk.itseats.member.entity;
+
+import com.idukbaduk.itseats.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "favorite")
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "favorite_id")
+    private Long favoriteId;
+}
