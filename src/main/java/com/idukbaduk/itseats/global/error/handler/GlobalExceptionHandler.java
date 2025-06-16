81c02bc2d83ce207afe68763836ea293f3000113
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-16 09:57:20 +0900

    feat: GlobalExceptionHandler 및 에러 코드/응답 클래스 추가

---
+++ src/main/java/com/idukbaduk/itseats/global/error/handler/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.idukbaduk.itseats.global.error.handler;
+
+import com.idukbaduk.itseats.global.error.core.BaseException;
+import com.idukbaduk.itseats.global.error.core.ErrorCode;
+import com.idukbaduk.itseats.global.error.core.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+        return getErrorResponse(e, e.getErrorCode());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return getErrorResponse(e, GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return getErrorResponse(e, GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, GlobalErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode, e.getMessage()));
+    }
+}
