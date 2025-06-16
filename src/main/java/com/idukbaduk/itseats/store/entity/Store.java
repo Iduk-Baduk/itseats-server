21fad870486a0934efc70cbe9ed56a12aae29730
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-16 17:31:13 +0900

    feat: Store 클래스 관련 연관관계 매핑

--- src/main/java/com/idukbaduk/itseats/store/entity/Store.java
+++ src/main/java/com/idukbaduk/itseats/store/entity/Store.java
@@ -1,10 +1,12 @@
 package com.idukbaduk.itseats.store.entity;
 
 import com.idukbaduk.itseats.global.BaseEntity;
+import com.idukbaduk.itseats.member.entity.Member;
 import com.idukbaduk.itseats.store.entity.enums.BusinessStatus;
 import com.idukbaduk.itseats.store.entity.enums.StoreStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.Fetch;
 import org.springframework.data.geo.Point;
 
 @Entity
@@ -16,6 +18,18 @@ public class Store extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id")
     private Long storeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_category_id", nullable = false)
+    private StoreCategory storeCategory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "franchise_id")
+    private Franchise franchise;
 
     @Column(name = "store_name", nullable = false)
     private String storeName;
