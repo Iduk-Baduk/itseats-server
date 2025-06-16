a804116a0ea6a4b998c34bdb3b4f20dbea87ffdb
sseen2 <kse10023@naver.com>
2025-06-16 14:34:42 +0900

    feat: 메뉴 이미지 entity 구현

---
+++ src/main/java/com/idukbaduk/itseats/review/entity/ReviewImage.java
@@ -0,0 +1,33 @@
+package com.idukbaduk.itseats.review.entity;
+
+import com.idukbaduk.itseats.global.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review_image")
+public class ReviewImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_image_id", nullable = false)
+    private Long reviewImageId;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "display_order", nullable = false)
+    private int displayOrder;
+}
