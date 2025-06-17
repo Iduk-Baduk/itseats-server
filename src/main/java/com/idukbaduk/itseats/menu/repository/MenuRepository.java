81d560814e8e0cc1d1b08dbcd02ca37cd945618b
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-17 15:57:08 +0900

    feat: 메뉴 조회 관련 repository 작성

---
+++ src/main/java/com/idukbaduk/itseats/menu/repository/MenuRepository.java
@@ -0,0 +1,22 @@
+package com.idukbaduk.itseats.menu.repository;
+
+import com.idukbaduk.itseats.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("SELECT m FROM Menu m " +
+            "JOIN FETCH m.menuGroup mg " +
+            "WHERE mg.store.storeId = :storeId " +
+            "AND (:menuGroup IS NULL OR m.menuGroup.menuGroupName = :menuGroup) " +
+            "AND (:keyword IS NULL OR m.menuName LIKE %:keyword%)")
+    List<Menu> findMenusByStore(
+            @Param("storeId") Long storeId,
+            @Param("menuGroup") String menuGroup,
+            @Param("keyword") String keyword
+    );
+}
