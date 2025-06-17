912667de11b0d0510150c822de654b62329e5d8f
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-17 15:56:42 +0900

    feat: 메뉴 조회 관련 Dto 작성

---
+++ src/main/java/com/idukbaduk/itseats/menu/dto/MenuListRequest.java
@@ -0,0 +1,16 @@
+package com.idukbaduk.itseats.menu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuListRequest {
+    private Long storeId;
+    private String menuGroup;
+    private String keyword;
+}
