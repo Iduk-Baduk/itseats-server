3a57df50b294b8c90c8b1b91bf31b6aac4341fa9
sseen2 <kse10023@naver.com>
2025-06-19 17:23:01 +0900

    feat: 주문 정보 상세 조회 관련 exception 추가

---
+++ src/main/java/com/idukbaduk/itseats/order/error/enums/OrderErrorCode.java
@@ -0,0 +1,25 @@
+package com.idukbaduk.itseats.order.error.enums;
+
+import com.idukbaduk.itseats.global.error.core.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum OrderErrorCode implements ErrorCode {
+
+    MENU_OPTION_SERIALIZATION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "menuOption 직렬화 실패"),
+    ORDER_NUMBER_CREATE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "중복되지 않는 주문번호 생성 실패");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[ORDER ERROR] " + message;
+    }
+}
