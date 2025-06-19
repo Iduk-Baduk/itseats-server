c1346069cd2796165d05220d70221e624817a78b
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-19 15:29:02 +0900

    feat: 메뉴 옵션 조회 관련 controller 추가

---
+++ src/main/java/com/idukbaduk/itseats/menu/controller/UserMenuController.java
@@ -0,0 +1,30 @@
+package com.idukbaduk.itseats.menu.controller;
+
+import com.idukbaduk.itseats.global.response.BaseResponse;
+import com.idukbaduk.itseats.menu.dto.UserMenuOptionResponse;
+import com.idukbaduk.itseats.menu.service.UserMenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stores")
+public class UserMenuController {
+
+    private final UserMenuService userMenuService;
+
+    @GetMapping("/{storeId}/{menuId}/options")
+    public ResponseEntity<BaseResponse> getMenuOptions(
+            @PathVariable Long storeId,
+            @PathVariable Long menuId
+    ) {
+        UserMenuOptionResponse response = userMenuService.getMenuOptions(menuId);
+
+        return BaseResponse.toResponseEntity(HttpStatus.OK, "메뉴 옵션 조회 성공", response);
+    }
+}
