3c6891457caae980cfa62fa72ba5d26972571b23
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-20 18:27:30 +0900

    feat: 가게 전체 조회 관련 Dto 추가

---
+++ src/main/java/com/idukbaduk/itseats/store/dto/StoreListResponse.java
@@ -0,0 +1,12 @@
+package com.idukbaduk.itseats.store.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class StoreListResponse {
+    private List<StoreDto> stores;
+}
