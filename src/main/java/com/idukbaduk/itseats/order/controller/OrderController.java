32c672b099ed2ae9d60852292003371ae487a522
sseen2 <kse10023@naver.com>
2025-06-20 13:14:29 +0900

    refactor: response 필드명 수정 및 불필요한 어노테이션 삭제

--- src/main/java/com/idukbaduk/itseats/order/controller/OrderController.java
+++ src/main/java/com/idukbaduk/itseats/order/controller/OrderController.java
@@ -5,11 +5,11 @@ import com.idukbaduk.itseats.order.dto.OrderNewRequest;
 import com.idukbaduk.itseats.order.dto.enums.OrderResponse;
 import com.idukbaduk.itseats.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +25,9 @@ public class OrderController {
     @PostMapping("/new")
     public ResponseEntity<BaseResponse> getOrderNew(
             @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody @Validated OrderNewRequest orderNewRequest) {
+            @RequestBody OrderNewRequest orderNewRequest) {
         return BaseResponse.toResponseEntity(
-                OrderResponse.SUCCESS_ORDER_DETAILS,
+                OrderResponse.GET_ORDER_DETAILS_SUCCESS,
                 orderService.getOrderNew(userDetails.getUsername(), orderNewRequest)
         );
     }
--- src/main/java/com/idukbaduk/itseats/order/dto/enums/OrderResponse.java
+++ src/main/java/com/idukbaduk/itseats/order/dto/enums/OrderResponse.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderResponse implements Response {
 
-    SUCCESS_ORDER_DETAILS(HttpStatus.OK, "주문 정보 상세 조회 성공");
+    GET_ORDER_DETAILS_SUCCESS(HttpStatus.OK, "주문 정보 상세 조회 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
