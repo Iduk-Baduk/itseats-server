7ef6af75e2d1d11c120074a6cd89573eaa375238
sseen2 <kse10023@naver.com>
2025-06-20 11:58:51 +0900

    feat: 주소 추가 service 추가

--- src/main/java/com/idukbaduk/itseats/memberaddress/service/MemberAddressService.java
+++ src/main/java/com/idukbaduk/itseats/memberaddress/service/MemberAddressService.java
@@ -1,10 +1,16 @@
 package com.idukbaduk.itseats.memberaddress.service;
 
+import com.idukbaduk.itseats.member.entity.Member;
+import com.idukbaduk.itseats.member.service.MemberService;
+import com.idukbaduk.itseats.memberaddress.dto.AddressCreateRequest;
+import com.idukbaduk.itseats.memberaddress.dto.AddressCreateResponse;
 import com.idukbaduk.itseats.memberaddress.entity.MemberAddress;
+import com.idukbaduk.itseats.memberaddress.entity.enums.AddressCategory;
 import com.idukbaduk.itseats.memberaddress.error.MemberAddressException;
 import com.idukbaduk.itseats.memberaddress.error.enums.MemberAddressErrorCode;
 import com.idukbaduk.itseats.memberaddress.repository.MemberAddressRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +18,26 @@ import org.springframework.stereotype.Service;
 public class MemberAddressService {
 
     private final MemberAddressRepository memberAddressRepository;
+    private final MemberService memberService;
+
+    public AddressCreateResponse createAddress(String username, AddressCreateRequest addressCreateRequest) {
+        Member member = memberService.getMemberByUsername(username);
+
+        MemberAddress memberAddress = MemberAddress.builder()
+                .member(member)
+                .mainAddress(addressCreateRequest.getMainAddress())
+                .detailAddress(addressCreateRequest.getDetailAddress())
+                .location(new Point(addressCreateRequest.getLocationX(), addressCreateRequest.getLocationY()))
+                .addressCategory(AddressCategory.valueOf(addressCreateRequest.getAddressCategory()))
+                .build();
+        memberAddressRepository.save(memberAddress);
+
+        return AddressCreateResponse.builder()
+                .mainAddress(memberAddress.getMainAddress())
+                .detailAddress(memberAddress.getDetailAddress())
+                .addressCategory(memberAddress.getAddressCategory().name())
+                .build();
+    }
 
     public MemberAddress getMemberAddress(Long addressId) {
         return memberAddressRepository.findById(addressId)
