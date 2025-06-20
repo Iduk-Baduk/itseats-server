3dd35bb46ce70b2cc6628075179b3146a8de7d2c
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-20 18:27:52 +0900

    feat: 가게 전체 조회 관련 Repository 추가

--- src/main/java/com/idukbaduk/itseats/store/repository/StoreRepository.java
+++ src/main/java/com/idukbaduk/itseats/store/repository/StoreRepository.java
@@ -5,10 +5,13 @@ import com.idukbaduk.itseats.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     Optional<Store> findByMemberAndStoreId(Member member, Long storeId);
+
+    List<Store> findAllByDeletedFalse();
 }
