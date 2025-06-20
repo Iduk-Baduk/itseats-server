cffd2827868ac3123f4a593adcba3770ef828b5c
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-20 15:20:48 +0900

    feat: 리뷰 목록 조회 관련 Repository 작성

---
+++ src/main/java/com/idukbaduk/itseats/review/repository/ReviewRepository.java
@@ -0,0 +1,21 @@
+package com.idukbaduk.itseats.review.repository;
+
+import com.idukbaduk.itseats.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("SELECT r FROM Review r " +
+            "JOIN FETCH r.member WHERE r.store.storeId = :storeId " +
+            "ORDER BY r.createdAt DESC")
+    List<Review> findReviewsWithMemberByStoreId(@Param("storeId") Long storeId);
+
+    @Query("SELECT r.store.storeId, COUNT(r) " +
+            "FROM Review r WHERE r.store.storeId " +
+            "IN :storeIds GROUP BY r.store.storeId")
+    List<Object[]> countReviewsByStoreIds(@Param("storeIds") List<Long> storeIds);
+
+}
