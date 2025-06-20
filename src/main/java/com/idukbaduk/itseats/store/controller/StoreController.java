5f926400fe1ea83878141c022f57b35804ed4f95
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-20 18:30:33 +0900

    feat: 가게 전체 조회 관련 Controller 추가

---
+++ src/main/java/com/idukbaduk/itseats/store/controller/StoreController.java
@@ -0,0 +1,25 @@
+package com.idukbaduk.itseats.store.controller;
+
+import com.idukbaduk.itseats.global.response.BaseResponse;
+import com.idukbaduk.itseats.store.dto.StoreListResponse;
+import com.idukbaduk.itseats.store.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stores")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @GetMapping("/list")
+    public ResponseEntity<BaseResponse> getAllStores() {
+        StoreListResponse response = storeService.getAllStores();
+        return BaseResponse.toResponseEntity(HttpStatus.OK, "전체 가게 목록 조회 성공", response);
+    }
+}
