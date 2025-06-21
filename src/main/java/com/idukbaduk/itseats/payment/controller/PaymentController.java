a1c20c6f49e71b657e70c5a730d84054c6028ceb
sseen2 <kse10023@naver.com>
2025-06-21 23:58:19 +0900

    feat: 결제 관련 controller 로직 추가

---
+++ src/main/java/com/idukbaduk/itseats/payment/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.idukbaduk.itseats.payment.controller;
+
+import com.idukbaduk.itseats.global.response.BaseResponse;
+import com.idukbaduk.itseats.payment.dto.PaymentInfoRequest;
+import com.idukbaduk.itseats.payment.dto.enums.PaymentResponse;
+import com.idukbaduk.itseats.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse> createPayment(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody PaymentInfoRequest paymentInfoRequest) {
+        return BaseResponse.toResponseEntity(
+                PaymentResponse.CREATE_PAYMENT_SUCCESS,
+                paymentService.createPayment(userDetails.getUsername(), paymentInfoRequest)
+        );
+    }
+}
