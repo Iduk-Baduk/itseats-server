ca5549482aef03c9e4dead2c191afd38d4bd558c
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-23 16:04:00 +0900

    feat: 대시보드 조회 관련 Repository 작성

--- src/main/java/com/idukbaduk/itseats/order/repository/OrderRepository.java
+++ src/main/java/com/idukbaduk/itseats/order/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -37,4 +38,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Long findAvgDeliveryTimeByType(@Param("deliveryType") String deliveryType);
 
     Optional<Order> findByMemberAndOrderId(Member member, Long orderId);
+
+    List<Order> findAllByStore_StoreId(Long storeStoreId);
 }
