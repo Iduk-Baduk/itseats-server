e550b0baf6359ab8d0ee2b0e9c90a93e2cf87625
sseen2 <kse10023@naver.com>
2025-06-23 16:25:55 +0900

    refactor: HTTP 상태코드 수정

--- src/main/java/com/idukbaduk/itseats/rider/dto/enums/RiderResponse.java
+++ src/main/java/com/idukbaduk/itseats/rider/dto/enums/RiderResponse.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum RiderResponse implements Response {
 
-    MODIFY_IS_WORKING_SUCCESS(HttpStatus.CREATED, "출/퇴근 상태전환 성공");
+    MODIFY_IS_WORKING_SUCCESS(HttpStatus.OK, "출/퇴근 상태전환 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
