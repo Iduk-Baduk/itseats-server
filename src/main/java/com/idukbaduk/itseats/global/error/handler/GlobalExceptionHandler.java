befe04bffb283ef01831e7c8d460fc063a66ec38
sseen2 <kse10023@naver.com>
2025-06-23 16:05:24 +0900

    feat: @Valid 관련 예외 핸들러 추가

--- src/main/java/com/idukbaduk/itseats/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/idukbaduk/itseats/global/error/handler/GlobalExceptionHandler.java
@@ -4,11 +4,23 @@ import com.idukbaduk.itseats.global.error.core.BaseException;
 import com.idukbaduk.itseats.global.error.core.ErrorCode;
 import com.idukbaduk.itseats.global.error.core.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
+        return ResponseEntity
+                .status(GlobalErrorCode.INVALID_INPUT_VALUE.getStatus())
+                .body(ErrorResponse.of(GlobalErrorCode.INVALID_INPUT_VALUE, errorMessage));
+    }
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
         return getErrorResponse(e, e.getErrorCode());
