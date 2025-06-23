3d666d6e231de32b75943fe084011639256faa41
sseen2 <kse10023@naver.com>
2025-06-23 22:48:21 +0900

    refactor: 주문 상태 추가 및 주석 추가

--- src/main/java/com/idukbaduk/itseats/order/entity/enums/OrderStatus.java
+++ src/main/java/com/idukbaduk/itseats/order/entity/enums/OrderStatus.java
@@ -1,11 +1,21 @@
 package com.idukbaduk.itseats.order.entity.enums;
 
 public enum OrderStatus {
+
+    // 주문 취소
+    CANCELED,
+    // 주문 접수 중
     WAITING,
+    // 주문 조리 (주문 접수)
     COOKING,
+    // 조리 완료 (라이더 배차 시작)
+    COOKED,
+    // 배차 완료
     RIDER_READY,
+    // 배달 시작
     DELIVERING,
+    // 배달 완료
     DELIVERED,
-    COMPLETED,
-    CANCELED
+    // 주문 완료
+    COMPLETED
 }
