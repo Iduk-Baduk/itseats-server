1fc41af3bcc80119d8cfe827f2e03754a0c807fa
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-24 11:24:41 +0900

    feat: 주문 접수 조회 관련 DTO 추가

---
+++ src/main/java/com/idukbaduk/itseats/order/dto/OrderReceptionResponse.java
@@ -0,0 +1,19 @@
+package com.idukbaduk.itseats.order.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class OrderReceptionResponse {
+    private String orderNumber;
+    private String orderTime;
+    private int menuCount;
+    private int totalPrice;
+    private List<OrderReceptionDTO> menuItems;
+    private String orderStatus;
+    private String customerRequest;
+    private String riderPhone;
+}
