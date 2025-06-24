3b4c349f9b5f1bddf8461d259797b9851e024f22
Woochang Lee <changi112242@gmail.com>
2025-06-24 14:24:03 +0900

    fix: WGS-84 (SRID 4326) 좌표계 설정

--- src/main/java/com/idukbaduk/itseats/global/util/GeoUtil.java
+++ src/main/java/com/idukbaduk/itseats/global/util/GeoUtil.java
@@ -3,9 +3,10 @@ package com.idukbaduk.itseats.global.util;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 
 public class GeoUtil {
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
+    private static final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
 
     /**
      * 경도/위도를 Point 객체로 변환한다.
