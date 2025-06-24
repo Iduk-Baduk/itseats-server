dfd84001c27bd936d8be02f455b0e6f90a3ebd47
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-24 11:24:12 +0900

    feat: Fetch Join을 위한 양방향 매핑

--- src/main/java/com/idukbaduk/itseats/order/entity/Order.java
+++ src/main/java/com/idukbaduk/itseats/order/entity/Order.java
@@ -6,17 +6,7 @@ import com.idukbaduk.itseats.order.entity.enums.OrderStatus;
 import com.idukbaduk.itseats.order.entity.enums.DeliveryType;
 import com.idukbaduk.itseats.rider.entity.Rider;
 import com.idukbaduk.itseats.store.entity.Store;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,6 +14,7 @@ import lombok.NoArgsConstructor;
 
 import org.springframework.data.geo.Point;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -87,4 +78,7 @@ public class Order extends BaseEntity {
 
     @Column(name = "order_end_time")
     private LocalDateTime orderEndTime;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    private List<OrderMenu> orderMenus;
 }
