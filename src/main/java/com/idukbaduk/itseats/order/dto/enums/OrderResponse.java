bbca81c65e083fe9512fdf88c49a68e9435f33b7
sseen2 <kse10023@naver.com>
2025-06-25 00:06:55 +0900

    feat: 라이더 주문 정보 조회 response DTO 추가

--- src/main/java/com/idukbaduk/itseats/order/dto/enums/OrderResponse.java
+++ src/main/java/com/idukbaduk/itseats/order/dto/enums/OrderResponse.java
@@ -10,7 +10,7 @@ public enum OrderResponse implements Response {
     GET_ORDER_DETAILS_SUCCESS(HttpStatus.OK, "주문 정보 상세 조회 성공"),
     GET_ORDER_STATUS_SUCCESS(HttpStatus.OK, "주문 현황 조회 성공"),
     GET_STORE_ORDERS_SUCCESS(HttpStatus.OK, "주문 접수 조회 성공"),
-    ;
+    GET_RIDER_ORDER_DETAILS_SUCCESS(HttpStatus.OK, "주문 정보 조회 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
