4062f6a4901747f1fc7d0a5c4236321f21df6c14
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-25 14:28:39 +0900

    feat: 조리 완료 관련 DTO 작성

---
+++ src/main/java/com/idukbaduk/itseats/order/dto/OrderCookedResponse.java
@@ -0,0 +1,10 @@
+package com.idukbaduk.itseats.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderCookedResponse {
+    private boolean success;
+}
