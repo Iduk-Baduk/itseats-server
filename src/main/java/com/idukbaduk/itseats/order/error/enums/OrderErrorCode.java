e10ed06f2de2cddac9cddbc47cf9740d191fa2d5
sseen2 <kse10023@naver.com>
2025-06-25 22:50:15 +0900

    feat: 배달 상태 촬영 service 로직 추가

--- src/main/java/com/idukbaduk/itseats/order/error/enums/OrderErrorCode.java
+++ src/main/java/com/idukbaduk/itseats/order/error/enums/OrderErrorCode.java
@@ -12,7 +12,7 @@ public enum OrderErrorCode implements ErrorCode {
     ORDER_STATUS_UPDATE_FAIL(HttpStatus.CONFLICT, "주문 상태 변경 실패"),
     ORDER_ALREADY_ASSIGNED(HttpStatus.CONFLICT, "해당 주문은 이미 라이더가 배정되었습니다."),
     INVALID_ORDER_STATUS(HttpStatus.CONFLICT, "잘못된 주문 상태입니다."),
-    ;
+    REQUIRED_RIDER_IMAGE(HttpStatus.BAD_REQUEST, "배달 상태 이미지는 필수입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
