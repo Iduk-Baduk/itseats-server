a5bd0b1a490d04808f3a20d836244fc495d70b4e
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-06-26 10:26:34 +0900

    feat: 가맹점 메뉴 상세 조회 관련 Service 추가

--- src/main/java/com/idukbaduk/itseats/menu/service/MenuService.java
+++ src/main/java/com/idukbaduk/itseats/menu/service/MenuService.java
@@ -67,6 +67,23 @@ public class MenuService {
                 .build();
     }
 
+    public MenuDetailResponse getMenuDetail(Long storeId, Long menuId) {
+        Menu menu = menuRepository.findDetailById(menuId)
+                .orElseThrow(() -> new MenuException(MenuErrorCode.MENU_NOT_FOUND));
+
+        return MenuDetailResponse.builder()
+                .menuId(menu.getMenuId())
+                .menuName(menu.getMenuName())
+                .menuDescription(menu.getMenuDescription())
+                .menuPrice(menu.getMenuPrice())
+                .menuStatus(menu.getMenuStatus().name())
+                .menuRating(menu.getMenuRating())
+                .menuGroupName(menu.getMenuGroup().getMenuGroupName())
+                .optionGroups(optionGroupsToDto(menu.getMenuOptionGroups()))
+                .build();
+    }
+
+
     private int getMenuCount(List<Menu> menus, MenuStatus menuStatus) {
         return (int) menus.stream()
                 .filter(m -> m.getMenuStatus() == menuStatus)
