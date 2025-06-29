5dd74a8eb3ef0d2980e55f6930a9cd6d781af1ea
박근석 <parkrootseok@gmail.com>
2025-06-29 11:58:44 +0900

    refactor: 비밀번호, 핸드폰 번호 패턴 추가 (#135)

--- src/main/java/com/idukbaduk/itseats/member/dto/request/CustomerCreateRequest.java
+++ src/main/java/com/idukbaduk/itseats/member/dto/request/CustomerCreateRequest.java
@@ -3,15 +3,25 @@ package com.idukbaduk.itseats.member.dto.request;
 import com.idukbaduk.itseats.member.dto.CustomerDto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public record CustomerCreateRequest(
         @NotBlank String username,
-        @NotBlank String password,
+
+        @NotBlank @Pattern(
+                regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "비밀번호는 최소 8자리, 영문, 숫자, 특수문자를 포함해야 합니다"
+        ) String password,
+
         @NotBlank String nickname,
+
         @Email String email,
-        @NotBlank String phone
+
+        @NotBlank @Pattern(
+                regexp = "^01[0-9]-\\d{4}-\\d{4}$", message = "올바른 휴대폰 번호 형식이 아닙니다"
+        ) String phone
 )
 {
     public CustomerDto toDto() {
@@ -23,4 +33,5 @@ public record CustomerCreateRequest(
                 .phone(this.phone)
                 .build();
     }
+
 }
