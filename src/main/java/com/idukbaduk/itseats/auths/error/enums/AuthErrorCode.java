d29693d1b587cb73b1c0c27c53be94cf24654522
박근석 <parkrootseok@gmail.com>
2025-06-30 19:02:19 +0900

    chore: 에러 코드 메시지 수정  (#150)

--- src/main/java/com/idukbaduk/itseats/auths/error/enums/AuthErrorCode.java
+++ src/main/java/com/idukbaduk/itseats/auths/error/enums/AuthErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 
-    AUTHORIZATION_PARSE_FAILED(HttpStatus.BAD_REQUEST, "아이디를 입력해주세요."),
+    AUTHORIZATION_PARSE_FAILED(HttpStatus.BAD_REQUEST, "로그인 정보를 올바르게 입력해주세요."),
     AUTHORIZATION_USERNAME_EMPTY(HttpStatus.BAD_REQUEST, "아이디를 입력해주세요."),
     AUTHORIZATION_PASSWORD_EMPTY(HttpStatus.BAD_REQUEST, "비밀번호를 입력해주세요.");
     private final HttpStatus httpStatus;
