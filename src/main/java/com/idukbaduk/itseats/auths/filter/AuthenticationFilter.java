6f81ca28ade1e22003810f1fc131b06d9a52811e
박근석 <parkrootseok@gmail.com>
2025-06-30 18:51:39 +0900

    refactor: 불필요한 클래스 제거 (#150)

--- src/main/java/com/idukbaduk/itseats/auths/filter/AuthenticationFilter.java
+++ src/main/java/com/idukbaduk/itseats/auths/filter/AuthenticationFilter.java
@@ -4,47 +4,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.idukbaduk.itseats.auths.dto.request.LoginRequest;
 import com.idukbaduk.itseats.auths.error.AuthException;
 import com.idukbaduk.itseats.auths.error.enums.AuthErrorCode;
-import com.idukbaduk.itseats.auths.service.AuthService;
 import com.idukbaduk.itseats.external.jwt.service.JwtTokenService;
-import com.idukbaduk.itseats.member.entity.Member;
-import com.idukbaduk.itseats.member.service.MemberService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    private final MemberService memberService;
     private final JwtTokenService jwtTokenService;
 
     public AuthenticationFilter(
-            AuthenticationManager authenticationManager, MemberService memberService, JwtTokenService jwtTokenService
+            AuthenticationManager authenticationManager, JwtTokenService jwtTokenService
     ) {
         super(authenticationManager);
-        this.memberService = memberService;
         this.jwtTokenService = jwtTokenService;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthException {
         try {
-            LoginRequest credential = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+            LoginRequest credential = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
 
             if (Objects.isNull(credential.username()) || credential.username().isBlank()) {
                 throw new AuthException(AuthErrorCode.AUTHORIZATION_USERNAME_EMPTY);
