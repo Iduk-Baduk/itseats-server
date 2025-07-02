010a12505a4da24fccd2c1553fdba7048523878a
Woochang Lee <changi112242@gmail.com>
2025-07-02 23:50:07 +0900

    feat: 과거 주문 조회 controller 로직 추가

--- src/main/java/com/idukbaduk/itseats/order/controller/OrderController.java
+++ src/main/java/com/idukbaduk/itseats/order/controller/OrderController.java
@@ -5,15 +5,12 @@ import com.idukbaduk.itseats.order.dto.OrderNewRequest;
 import com.idukbaduk.itseats.order.dto.enums.OrderResponse;
 import com.idukbaduk.itseats.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -29,6 +26,18 @@ public class OrderController {
         return BaseResponse.toResponseEntity(
                 OrderResponse.GET_ORDER_DETAILS_SUCCESS,
                 orderService.getOrderNew(userDetails.getUsername(), orderNewRequest)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse> getOrders(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam(defaultValue = "") String keyword,
+            @PageableDefault(page = 0, size = 20) Pageable pageable
+    ) {
+        return BaseResponse.toResponseEntity(
+                OrderResponse.GET_ORDERS_SUCCESS,
+                orderService.getOrders(userDetails.getUsername(), keyword, pageable)
         );
     }
 
