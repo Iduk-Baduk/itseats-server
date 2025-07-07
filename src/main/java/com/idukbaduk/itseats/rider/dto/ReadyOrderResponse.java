13d8adb8f3ac62c62b562fa7bb535e7e62c9a31f
한상희 <tkdgml822@gmail.com>
2025-07-08 01:09:44 +0900

    refactor: 배달 조회 Response Getter 추가

--- src/main/java/com/idukbaduk/itseats/rider/dto/ReadyOrderResponse.java
+++ src/main/java/com/idukbaduk/itseats/rider/dto/ReadyOrderResponse.java
@@ -1,8 +1,10 @@
 package com.idukbaduk.itseats.rider.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ReadyOrderResponse{
     private String deliveryType;
     private String storeName;
