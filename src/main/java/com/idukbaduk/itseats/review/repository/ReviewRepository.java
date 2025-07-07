44d83fc52da62e8255b08ef34230a29200f86c91
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-07-07 13:40:36 +0900

    feat: 특정 기간 내 가게 별 리뷰 조회 관련 쿼리문 작성

--- src/main/java/com/idukbaduk/itseats/review/repository/ReviewRepository.java
+++ src/main/java/com/idukbaduk/itseats/review/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -22,4 +23,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT COUNT(r) FROM Review r WHERE r.store.storeId = :storeId")
     int countByStoreId(@Param("storeId") Long storeId);
+
+    List<Review> findByStore_StoreIdAndCreatedAtBetween(Long storeId, LocalDateTime start, LocalDateTime end);
+
 }
