4ebadd948daedb5bb3816b1c5f3711bfd79407cb
한상희 <tkdgml822@gmail.com>
2025-07-07 17:16:31 +0900

    fix: Repository 배달 조회 반환값 수정

--- src/main/java/com/idukbaduk/itseats/order/repository/OrderRepository.java
+++ src/main/java/com/idukbaduk/itseats/order/repository/OrderRepository.java
@@ -156,7 +156,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             WHERE distance <= :radiusInMeters
             ORDER BY distance;
     """, nativeQuery = true)
-    Optional<List<NearbyOrderDTO>> findNearbyOrders(
+    List<NearbyOrderDTO> findNearbyOrders(
             @Param("lat") double latitude,
             @Param("lon") double longitude,
             @Param("radiusInMeters") int radiusInMeters
