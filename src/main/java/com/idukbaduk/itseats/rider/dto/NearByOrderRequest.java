7750f8a539f96b77b11484e1fb27ac86b20b131b
한상희 <tkdgml822@gmail.com>
2025-07-08 01:14:39 +0900

    refactor: 위도, 경도 유효성 검사 추가

--- src/main/java/com/idukbaduk/itseats/rider/dto/NearByOrderRequest.java
+++ src/main/java/com/idukbaduk/itseats/rider/dto/NearByOrderRequest.java
@@ -1,5 +1,7 @@
 package com.idukbaduk.itseats.rider.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.DecimalMax;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class NearByOrderRequest {
+
+    @DecimalMin(value = "-90.0", message = "위도는 -90도 이상이어야 합니다.")
+    @DecimalMax(value = "90.0", message = "위도는 90도 이하여야 합니다.")
     private double latitude;
+
+    @DecimalMin(value = "-180.0", message = "경도는 -180도 이상이어야 합니다")
+    @DecimalMax(value = "180.0", message = "경도는 180도 이하여야 합니다")
     private double longitude;
 
     public NearByOrderRequest(double latitude, double longitude) {
