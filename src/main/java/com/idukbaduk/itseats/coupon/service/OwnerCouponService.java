6cdc5e09ede0f0615bf46f82cfd489966a27a042
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-07-08 14:22:54 +0900

    feat: 프랜차이즈 쿠폰 생성 관련 Service 작성

--- src/main/java/com/idukbaduk/itseats/coupon/service/OwnerCouponService.java
+++ src/main/java/com/idukbaduk/itseats/coupon/service/OwnerCouponService.java
@@ -1,13 +1,16 @@
 package com.idukbaduk.itseats.coupon.service;
 
 import com.idukbaduk.itseats.coupon.dto.CouponCreateRequest;
+import com.idukbaduk.itseats.coupon.dto.FranchiseCouponCreateResponse;
 import com.idukbaduk.itseats.coupon.dto.StoreCouponCreateResponse;
 import com.idukbaduk.itseats.coupon.entity.Coupon;
 import com.idukbaduk.itseats.coupon.entity.enums.TargetType;
 import com.idukbaduk.itseats.coupon.repository.CouponRepository;
+import com.idukbaduk.itseats.store.entity.Franchise;
 import com.idukbaduk.itseats.store.entity.Store;
 import com.idukbaduk.itseats.store.error.StoreException;
 import com.idukbaduk.itseats.store.error.enums.StoreErrorCode;
+import com.idukbaduk.itseats.store.repository.FranchiseRepository;
 import com.idukbaduk.itseats.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,7 @@ public class OwnerCouponService {
 
     private final StoreRepository storeRepository;
     private final CouponRepository couponRepository;
+    private final FranchiseRepository franchiseRepository;
 
     @Transactional
     public StoreCouponCreateResponse createStoreCoupon(Long storeId, CouponCreateRequest request, String username) {
@@ -46,6 +50,40 @@ public class OwnerCouponService {
         Coupon savedCoupon = couponRepository.save(coupon);
 
         return StoreCouponCreateResponse.builder()
+                .couponId(savedCoupon.getCouponId())
+                .name(savedCoupon.getCouponName())
+                .quantity(savedCoupon.getQuantity())
+                .couponType(savedCoupon.getCouponType())
+                .minPrice(savedCoupon.getMinPrice())
+                .discountValue(savedCoupon.getDiscountValue())
+                .issueStartDate(savedCoupon.getIssueStartDate())
+                .validDate(savedCoupon.getValidDate())
+                .build();
+    }
+
+    @Transactional
+    public FranchiseCouponCreateResponse createFranchiseCoupon(Long franchiseId, CouponCreateRequest request) {
+
+        Franchise franchise = franchiseRepository.findById(franchiseId)
+                .orElseThrow(() -> new StoreException(StoreErrorCode.FRANCHISE_NOT_FOUND));
+
+        Coupon coupon = Coupon.builder()
+                .franchise(franchise)
+                .couponName(request.getName())
+                .description(request.getDescription())
+                .discountValue(request.getDiscountValue())
+                .couponType(request.getCouponType())
+                .quantity(request.getQuantity())
+                .minPrice(request.getMinPrice())
+                .issueStartDate(request.getIssueStartDate())
+                .validDate(request.getValidDate())
+                .targetType(TargetType.FRANCHISE)
+                .build();
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+
+        return FranchiseCouponCreateResponse.builder()
+                .franchiseName(franchise.getBrandName())
                 .couponId(savedCoupon.getCouponId())
                 .name(savedCoupon.getCouponName())
                 .quantity(savedCoupon.getQuantity())
