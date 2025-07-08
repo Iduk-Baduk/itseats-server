b5d75bb741bbddbc0212d01dd70f4fe695f3dc8d
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-07-08 16:58:20 +0900

    feat: 쿠폰 발급 관련 Controller 작성

---
+++ src/main/java/com/idukbaduk/itseats/coupon/dto/enums/CouponResponse.java
@@ -0,0 +1,25 @@
+package com.idukbaduk.itseats.coupon.dto.enums;
+
+import com.idukbaduk.itseats.global.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum CouponResponse implements Response {
+
+    ISSUE_COUPON_SUCCESS(HttpStatus.CREATED, "쿠폰이 발급되었습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
