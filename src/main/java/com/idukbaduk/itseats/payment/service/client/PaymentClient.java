f40b4697a8fa182a33bd33b80e6647e79395a4ec
sseen2 <kse10023@naver.com>
2025-07-08 12:00:55 +0900

    feat: 토스 결제 상태 검증 로직 추가

--- src/main/java/com/idukbaduk/itseats/payment/service/client/PaymentClient.java
+++ src/main/java/com/idukbaduk/itseats/payment/service/client/PaymentClient.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 
+import java.util.Objects;
+
 @Component
 @RequiredArgsConstructor
 public class PaymentClient {
@@ -17,12 +19,20 @@ public class PaymentClient {
 
     public PaymentClientResponse confirmPayment(PaymentConfirmRequest confirmRequest) {
         try {
-            return restClient.post().uri("/confirm")
+            PaymentClientResponse clientResponse =  restClient.post().uri("/confirm")
                     .body(confirmRequest)
                     .retrieve()
                     .body(PaymentClientResponse.class);
+            validatePaymentStatus(Objects.requireNonNull(clientResponse).getStatus());
+            return clientResponse;
         } catch (ResourceAccessException e) {
             throw new PaymentException(PaymentErrorCode.TOSS_PAYMENT_SERVER_ERROR);
+        }
+    }
+
+    private void validatePaymentStatus(String status) {
+        if (!status.equals("DONE")) {
+            throw new PaymentException(PaymentErrorCode.PAYMENT_FAIL);
         }
     }
 }
