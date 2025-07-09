a3ebb3fd02fa27d455514bc6d09efd9e3134c3ab
sseen2 <kse10023@naver.com>
2025-07-09 11:16:12 +0900

    refactor: 결제 endpoint 수정

--- src/main/java/com/idukbaduk/itseats/payment/controller/PaymentController.java
+++ src/main/java/com/idukbaduk/itseats/payment/controller/PaymentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
