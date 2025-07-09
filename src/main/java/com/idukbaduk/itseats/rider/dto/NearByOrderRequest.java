32b25ec964b63becd186abcc5d3c78ae6c86ee92
한상희 <tkdgml822@gmail.com>
2025-07-09 22:57:19 +0900

    refactor: Request 생성자 삭제 및 룸북 어노테이션으로 교체

--- src/main/java/com/idukbaduk/itseats/rider/dto/NearByOrderRequest.java
+++ src/main/java/com/idukbaduk/itseats/rider/dto/NearByOrderRequest.java
@@ -3,6 +3,7 @@ package com.idukbaduk.itseats.rider.dto;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NearByOrderRequest {
 
     @NotNull(message = "위도는 필수값입니다.")
@@ -21,9 +23,4 @@ public class NearByOrderRequest {
     @DecimalMin(value = "-180.0", message = "경도는 -180도 이상이어야 합니다")
     @DecimalMax(value = "180.0", message = "경도는 180도 이하여야 합니다")
     private Double longitude;
-
-    public NearByOrderRequest(Double latitude, Double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
 }
