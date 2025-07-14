c58940bcde76e40e3f95ac576e8fd1809ac30876
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-07-14 10:22:53 +0900

    refactor: ReviewDto에 신고여부 필드 추가

--- src/main/java/com/idukbaduk/itseats/review/repository/ReviewReportRepository.java
+++ src/main/java/com/idukbaduk/itseats/review/repository/ReviewReportRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewReportRepository extends JpaRepository<ReviewReport, Long> {
     boolean existsByMember_MemberIdAndReview_ReviewId(Long memberId, Long reviewId);
+
+    boolean existsByReview_ReviewId(Long reviewId);
+
 }
