fde8a292427a82b332cfe2560f81e169d2ae79ba
Woochang Lee <changi112242@gmail.com>
2025-07-15 00:40:54 +0900

    feat: 주문 정보 조회 응답 필드 추가
    목적지 좌표, 주소

--- src/main/java/com/idukbaduk/itseats/order/dto/OrderItemDTO.java
+++ src/main/java/com/idukbaduk/itseats/order/dto/OrderItemDTO.java
@@ -1,5 +1,6 @@
 package com.idukbaduk.itseats.order.dto;
 
+import com.idukbaduk.itseats.order.entity.OrderMenu;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,4 +12,13 @@ public class OrderItemDTO {
     private int quantity;
     private int menuPrice;
     private String options;
+
+    public static OrderItemDTO of(OrderMenu orderMenu) {
+        return OrderItemDTO.builder()
+                .menuName(orderMenu.getMenuName())
+                .quantity(orderMenu.getQuantity())
+                .menuPrice(orderMenu.getPrice())
+                .options(orderMenu.getMenuOption())
+                .build();
+    }
 }
--- src/main/java/com/idukbaduk/itseats/order/dto/RiderOrderDetailsResponse.java
+++ src/main/java/com/idukbaduk/itseats/order/dto/RiderOrderDetailsResponse.java
@@ -1,5 +1,9 @@
 package com.idukbaduk.itseats.order.dto;
 
+import com.idukbaduk.itseats.global.util.GeoUtil;
+import com.idukbaduk.itseats.order.entity.Order;
+import com.idukbaduk.itseats.payment.entity.Payment;
+import com.idukbaduk.itseats.store.dto.PointDto;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,9 +18,31 @@ public class RiderOrderDetailsResponse {
     private String orderStatus;
     private String orderTime;
     private int totalPrice;
+    private int deliveryFee;
     private List<OrderItemDTO> orderItems;
     private String storePhone;
     private String memberPhone;
+    private String address;
+    private PointDto destination;
     private String storeRequest;
     private String riderRequest;
+
+    public static RiderOrderDetailsResponse of(Order order, Payment payment) {
+
+        return RiderOrderDetailsResponse.builder()
+                .orderId(order.getOrderId())
+                .orderNumber(order.getOrderNumber())
+                .orderStatus(order.getOrderStatus().name())
+                .orderTime(order.getCreatedAt().toString())
+                .totalPrice(order.getOrderPrice())
+                .deliveryFee(order.getDeliveryFee())
+                .orderItems(order.getOrderMenus().stream().map(OrderItemDTO::of).toList())
+                .storePhone(order.getStore().getStorePhone())
+                .memberPhone(order.getMember().getPhone())
+                .address(order.getDeliveryAddress())
+                .destination(new PointDto(order.getDestinationLocation()))
+                .storeRequest(payment.getStoreRequest())
+                .riderRequest(payment.getRiderRequest())
+                .build();
+    }
 }
